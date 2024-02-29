e5e4040d883d053956ed6bf2dc64e4da5da52cb9
frupreneur <frupreneur@gmail.com>
2024-02-29 10:42:48 +0100

    first

---
+++ db/index.ts
@@ -0,0 +1,20 @@
+type MenuType = {
+  name: string;
+  path: string;
+}[];
+
+export const menu: MenuType = [
+  { name: "Home", path: "/" },
+  {
+    name: "About Us",
+    path: `#`,
+  },
+  {
+    name: "Privacy Policy",
+    path: `#`,
+  },
+  {
+    name: "Contact us",
+    path: `#`,
+  },
+];
